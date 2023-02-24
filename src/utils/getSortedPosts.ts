863bbcbb09322d86aa1e075b56f82a6429429ac6
Tatsuhiko Kato <hastur@me.com>
2023-02-24 15:48:44 +0900

    Contentful integration (#2)
    * Adding contentful integration
    
    * finish contentful integration

--- src/utils/getSortedPosts.ts
+++ src/utils/getSortedPosts.ts
@@ -1,12 +1,18 @@
 import type { CollectionEntry } from "astro:content";
 
-const getSortedPosts = (posts: CollectionEntry<"blog">[]) =>
-  posts
-    .filter(({ data }) => !data.draft)
-    .sort(
-      (a, b) =>
-        Math.floor(new Date(b.data.pubDatetime).getTime() / 1000) -
-        Math.floor(new Date(a.data.pubDatetime).getTime() / 1000)
-    );
+const getSortedPosts = (
+  posts: {
+    title: string;
+    postSlug: string;
+    description: string;
+    pubDatetime: Date;
+    tags: string[];
+  }[]
+) =>
+  posts.sort(
+    (a, b) =>
+      Math.floor(new Date(b.pubDatetime).getTime() / 1000) -
+      Math.floor(new Date(a.pubDatetime).getTime() / 1000)
+  );
 
 export default getSortedPosts;
